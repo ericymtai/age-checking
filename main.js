77b13df729591d6711bb4cfff5c2d1bd58a261ac
ericytai <ericym2@gmail.com>
2016-09-18 19:22:09 -0400

    Add the loop and append child to order list.

--- main.js
+++ main.js
@@ -2,46 +2,55 @@ function outer() {
 
           var age = prompt("What is Your Ages? or press 'exit' to stop!");
 
-
           if (age === "exit") {
             console.log("exit");
-            document.getElementById("myDiv").innerHTML = "Thanks for enter your age!!";
+            // document.getElementById("Div").innerHTML = "Thanks for Checking your age!!";
           } else if  (isNaN(age) || age == "" ){
                 inner();
           } else  {
                 getAge(age) ;
           }
 
-      function inner() {
-            var age = prompt("Please enter numbers?");
+          function inner() {
+                var age = prompt("Please enter numbers?");
 
-            if ( isNaN(age) || age == "" ) {
-                  inner();
-            } else {
-                  getAge(age) ;
-            }
-      };
+                if ( isNaN(age) || age == "" ) {
+                      inner();
+                } else {
+                      getAge(age) ;
+                }
+          };
 };
 
 function getAge(age) {
 
-  var text = "You are old enough to ";
-  var action = {16: "drive!", 17: "drive!", 18: "drive and vote!", "other": "drive, vote and drink!!"};
+    var text = "You are old enough to ";
+    var action = {16: "drive!", 17: "drive!", 18: "drive and vote!", "other": "drive, vote and drink!!"};
 
-  if (age == 16 || age == 17 || age == 18) {
-        var x = age;
-        alert( text + action[x]);
-        outer();
-  } else if (age < 16) {
-        alert( "YOU ARE SO YOUNG!!");
-        outer();
-  } else {
-        var x = "other";
-        alert( text + action[x]);
-        outer();
-  }
+    if (age == 16 || age == 17 || age == 18) {
+          var x = age;
+          alert( text + action[x]);
+          outer();
+    } else if (age < 16) {
+          alert( "YOU ARE SO YOUNG!!");
+          outer();
+    } else {
+          var x = "other";
+          alert( text + action[x]);
+          outer();
+    }
+
+    var records = [];
+
+    records.push(age);
+    node = document.createElement("LI");
+
+    for (var i=0; i<=records.length-1; i++) {
+        textnode = document.createTextNode("Year: " + records[i]  + " checked, ");
+        node.appendChild(textnode);
+        document.getElementById("myDiv").appendChild(node);
+    }
 
 };
-
 
 outer();
